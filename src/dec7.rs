e0a4072987a43e4f770905ab9a18c1a240687d61
Joachim E. Nebel <joachim.e.nebel@gmail.com>
2024-12-07 17:12:34 +0100

    Formatting

--- src/dec7.rs
+++ src/dec7.rs
@@ -11,7 +11,10 @@ fn parse_input<'a>(input: &'a str) -> Vec<(u64, Vec<u64>)> {
 }
 
 pub fn part1(input: &str) -> u64 {
-    parse_input(input).iter().filter(|x| is_possible_add_mul(x.0, &x.1)).map(|x| x.0).sum()
+    parse_input(input).iter()
+                      .filter(|x| is_possible_add_mul(x.0, &x.1))
+                      .map(|x| x.0)
+                      .sum()
 }
 
 fn is_possible_add_mul(target: u64, nums: &[u64]) -> bool {
@@ -25,7 +28,10 @@ fn is_possible_add_mul(target: u64, nums: &[u64]) -> bool {
 }
 
 pub fn part2(input: &str) -> u64 {
-    parse_input(input).iter().filter(|x| is_possible_add_mul_comb(x.0, &x.1)).map(|x| x.0).sum()
+    parse_input(input).iter()
+                      .filter(|x| is_possible_add_mul_comb(x.0, &x.1))
+                      .map(|x| x.0)
+                      .sum()
 }
 
 fn is_possible_add_mul_comb(target: u64, nums: &[u64]) -> bool {
